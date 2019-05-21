bd6d3ad0266940cdbdf6ce940a2fa18a44378ec1
jarze <jarze@jarzedeMacBook-Air.local>
2019-05-22 02:25:46 +0800

    finish a job

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(nunmber) {
+    if (isNaN(nunmber)) {
+    	return;
+    }
+    let lcds = [['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']];
+    let lcd = [[],[],[]];
+    for (let i = 0; i < nunmber.length; i++) {
+    	let char = parseInt(nunmber[i]);
+    	lcd[0].push(lcds[0][char]);
+    	lcd[1].push(lcds[1][char]);
+    	lcd[2].push(lcds[2][char]);
+    }
+    for (let i = lcd.length - 1; i >= 0; i--) {
+    	lcd[i] = lcd[i].join(' ');
+    }
+    let lcdshow = lcd.join('\n') + '\n';
+    return lcdshow;
 };
